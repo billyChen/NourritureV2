38c56fded1b87b04349a8df0cdc5736986bb62b4
billyChen <chen.billy94@gmail.com>
2015-11-07 00:25:57 +0800

    remove passport module

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "https://nourritureapi.herokuapp.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
