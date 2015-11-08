7483c19f340a06e99285c6ec75939efe22bc6f29
billyChen <chen.billy94@gmail.com>
2015-11-08 14:52:49 +0800

    correct monk

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk(' mongodb://root:123456@ds049624.mongolab.com:49624/nourriture');
+var db = monk('mongodb://root:123456@ds049624.mongolab.com:49624/nourriture');
 
 var FACEBOOK_APP_ID = "322980324492560";
 var FACEBOOK_APP_SECRET = "745cc0ed81f3de714e42d6fd086abff5";
