f991ae8218a5cb910b33d6f0528b30863d436a54
billyChen <chen.billy94@gmail.com>
2015-11-23 15:36:48 +0800

    sprint 1

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://root:123456@ds049624.mongolab.com:49624/nourriture');
 
 var app = express();
 
@@ -21,9 +27,71 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'NourritureWebInterfaceSecret2015'}));
+
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+app.get('/createUser', function(req, res, next){
+  var db = req.db;
+  var collection = db.get('allergens');
+  var allergens = [''];
+  var ingredients = [''];
+  request.get({
+    url: 'http://nourritureapi.herokuapp.com/listAllergens',
+    method: 'GET',
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      allergens = JSON.parse(body);
+      request.get({
+        url: 'http://nourritureapi.herokuapp.com/listIngredients',
+        method: 'GET',
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          ingredients = JSON.parse(body);
+          res.render('createUser', { title: 'Create your profil',
+                     allergens: allergens,
+                     ingredients: ingredients});
+        }
+      });
+    }
+  });
+});
+
+app.post('/UserAuthentication', function(req, res, next){
+  var sess;
+
+  request.post({
+    url: 'http://nourritureapi.herokuapp.com/ensureAuth',
+    method: 'POST',
+    form: {
+      username: req.body.username,
+      password: req.body.password
+    }
+  }, function (error, response, body) {
+    sess = req.session;
+
+    sess.user = JSON.parse(body);
+    console.log(body);
+    res.redirect('/');
+  });
+});
+
+app.get('/logOut', function (req, res, next) {
+  req.session.destroy();
+});
+
+
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/welcome', users);
+app.use('/addIngredients', users);
+app.use('/addRecipe', users);
+app.use('/logIn', users);
+// app.use('/createUser', users);
 //app.use('/nourriture', routes);
 
 // catch 404 and forward to error handler
@@ -56,14 +124,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-app.get('/', function(req, res){
-  //res.send('salut tout le monde');
-  res.render('index', {title : 'accueil'});
-})
-.get('/nourriture', function(req, res){
-  res.render('nourriture', {title: 'nourriture'});
-});
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var sess = req.session;
+  res.render('index', { title: 'Home', sess: sess });
 });
 
 /* GET nourriture page. */
@@ -18,20 +19,64 @@ router.get('/result', function (req, res) {
 
 router.post('/query_search', function (req, res) {
   request.post({
-    url: 'https://nourritureapi.herokuapp.com/search',
+    url: 'http://nourritureapi.herokuapp.com/search',
     method: 'POST',
     form: {
-        search: req.body.q
+      search: req.body.q
     }
-}, function (error, response, body) {
+  }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var result = body;
-      console.log("==============================BODY==============================")
-      console.log(body);
     }
     res.send(body);
   });
 });
 
+/* GET connection page. */
+router.get('/welcome', function(req, res, next){
+	res.render('welcome', { title: 'bienvenu' });
+});
+
+router.get('/addIngredients', function(req, res, next){
+	res.render('addIngredients', { title: 'Add your ingredients !!!' });
+});
+
+router.get('/addRecipe', function(req, res, next){
+	res.render('addRecipe', { title: 'Add your recipe !!!' });
+});
+
+router.get('/logIn', function(req, res, next) {
+	res.render('logIn', { title: 'LogIn'});
+});
+
+router.post('/insertUserData', function(req, res, next){
+  var db = req.db;
+  var collection = db.get('users');
+  var obj = {};
+
+  collection.insert(req.body, function(err, result){
+    res.render('inscriptionSuccess',
+               (err === null) ? { msg: 'Success' } : { msg: err }
+               );
+  });
+});
+
+router.get('/getSuitability/:id', function(req, res, next){
+  if (req.session['user']) {
+    request.post({
+      url: 'http://nourritureapi.herokuapp.com/getSuitability',
+      method: 'POST',
+      form: {
+        _user: req.session['user'],
+        _id: req.params.id
+      }
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var result = body;
+      }
+      res.send(body);
+    });
+  }
+});
 
 module.exports = router;
