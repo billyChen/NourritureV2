f68434a6a165e2c25fda7d29649321c06d1afd8f
billyChen <chen.billy94@gmail.com>
2015-11-24 16:46:49 +0800

    alternative products revision

--- app.js
+++ app.js
@@ -450,10 +450,8 @@ app.post('/getAlternativeProducts', function (req, res) {
   if (req.body._user) {
     var user = req.body._user;
     var preferences = user[0]['preferences'];
-    var random_preference = preferences[0];
 
-    res.send(user);
-    console.log(random_preference);
+    res.send(preferences);
     // collection.find({'types' : random_preference}, {}, function (err, docs) {
 
     // });
