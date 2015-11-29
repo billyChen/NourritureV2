e757bd5a210bd6bdf5cb0e64106aae32a263ee35
billyChen <chen.billy94@gmail.com>
2015-11-30 00:23:38 +0800

    test passport facebook

--- app.js
+++ app.js
@@ -68,16 +68,17 @@ function(accessToken, refreshToken, profile, done) {
 
     collection.find({'_facebook_id': profile.id}, {}, function (e, user)
     {
+
      request.post(
      {
       url: 'http://nourritureapi.herokuapp.com/addUsers',
       method: 'POST',
       form:
       {
-        _facebook_id: profile.id,
         _access_token: accessToken,
-        profile: profile
-        }
+        profile: profile,
+        user: user
+      }
     },
     function (error, response, body)
     {
