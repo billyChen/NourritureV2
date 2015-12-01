61cdc64e6ca00bd3f449194a992485bb3dc46a44
billyChen <chen.billy94@gmail.com>
2015-12-01 15:39:43 +0800

    add google strategy

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ function(accessToken, refreshToken, profile, done) {
 
       collection.find({'_google_id': profile.id}, {}, function (e, user)
       {
+        if (user) {
+          return done(null, user);
+        }
+        else {
          request.post(
          {
           url: 'http://nourritureapi.herokuapp.com/addUsers',
@@ -67,6 +71,7 @@ function(accessToken, refreshToken, profile, done) {
         {
           return done(null, profile);
         });
+       }
      });
     });
   }
